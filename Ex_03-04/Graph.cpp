d3644fc858673163c11e93e4129aec09cc8b3a79
Rokas Labanauskas (labanau2) <labanau2@coventry.ac.uk>
2018-11-18 20:24:19 +0200

    3_ex finished, isPath implemented using BFS.

--- Ex_03-04/Graph.cpp
+++ Ex_03-04/Graph.cpp
@@ -2,6 +2,7 @@
 #include "Graph.h"
 #include <iostream>
 #include <vector>
+#include <queue>
 
 Graph::Graph(int vertices)
 {
@@ -19,13 +20,63 @@ void Graph::add_edge(int source, int destination)
 	this->adj_list[destination].push_back(source);
 }
 
+bool Graph::isPath(int vertice1, int vertice2)
+{	
+	std::queue<int> temp_queue;
+
+	std::vector<bool> vertices_visited(this->vertices);
+	
+	std::vector<int> parent(this->vertices);
+	std::vector<int> path;
+	std::vector<int>::iterator i; // Iteratpr used for the vector cycle at the bottom
+	//to get all vertices in the adj_list.
+
+	if (vertice1 == vertice2) {
+		return true;
+	}
+
+	for (int i = 0; i < this->vertices; i++) {
+		vertices_visited[i] = false;
+	}
+
+	vertices_visited[vertice1] = true; // Marking Current node as visited
+	temp_queue.push(vertice1);
+
+	while (!temp_queue.empty()) {
+		vertice1 = temp_queue.front();
+		temp_queue.pop();
+
+		for (i = adj_list[vertice1].begin(); i != adj_list[vertice1].end(); i++) {
+			if (*i == vertice2) {
+				parent[*i] = vertice2;
+				path.push_back(*i);
+				print_path(parent, path);
+				return true;
+			}
+			if (!vertices_visited[*i]) {
+				vertices_visited[*i] = true;
+				path.push_back(*i);
+				parent[*i] = vertice1;
+				temp_queue.push(*i);
+			}
+		}
+	}
+	return false;
+}
+
 void Graph::print_graph()
 {
 	for (int i = 0; i<this->vertices; i++) {
 		std::cout << i << " -> ";
-		for (auto edge : *adj_list) {
-			std::cout << " " << edge << " ";
+		for (int j = 0; j < adj_list[i].size(); j++) {
+			std::cout << adj_list[i][j] << " ";
 		}
 		std::cout << std::endl;
+	}
+}
+
+void Graph::print_path(std::vector<int> parent_list, std::vector<int> path_list) {
+	for (std::vector<int>::iterator i = path_list.begin(); i < path_list.end(); i++) {
+		std::cout << parent_list[*i] << std::endl;
 	}
 }
--- Ex_03-04/Graph.h
+++ Ex_03-04/Graph.h
@@ -12,6 +12,7 @@ public:
 	~Graph();
 
 	void add_edge( int source, int destination);
+	bool isPath( int vertice1, int vertice2);
+	void print_path(std::vector<int> parent_list, std::vector<int> path_list);
 	void print_graph();
 };
-
